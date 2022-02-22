697dab1f2f5e6606eaa4aeea6707a406dacf15a8
Jensen Tran <jensen@kyber.network>
2022-02-22 17:35:10 +0700

    fix deployment script

--- deployment/deployKs.js
+++ deployment/deployKs.js
@@ -1,15 +1,13 @@
-const DMMFactory = artifacts.require('KSFactoryV2');
+const DMMFactory = artifacts.require('KSFactory');
 const DMMRouter02 = artifacts.require('DMMRouter02');
 const BN = web3.utils.BN;
-const FACTOR_IN_PRECISION = new BN(0.3);
 
 async function main() {
   const wethAddress = '0xc778417e063141139fce010982780140aa0cd5ab';
   const accounts = await web3.eth.getAccounts();
-  const FACTOR_IN_PRECISION = new BN(0.3);
 
   // We get the contract to deploy
-  const factory = await DMMFactory.new(accounts[0], FACTOR_IN_PRECISION);
+  const factory = await DMMFactory.new(accounts[0]);
   console.log('Factory V2 deployed to:', factory.address);
 
   const router = await DMMRouter02.new(factory.address, wethAddress);
