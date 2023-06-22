d302dfcb8dacd1bd86fe75de90f9e452be46d311
Zohar97 <zohar@kyber.network>
2023-06-22 11:12:45 +0700

    add linea

--- deploy/deploy_Linea.js
+++ deploy/deploy_Linea.js
@@ -2,10 +2,11 @@ const hre = require("hardhat");
 
 async function main () {
   const wethAddress = '0x2C1b868d6596a18e32E61B901E4060C872647b6C';
-  const feeToS = '0x96202931a23e5349f88D7bCF422AA3e4B811C758';
+  const feeToS = '0x4f82e73EDb06d29Ff62C91EC8f5Ff06571bdeb29';
 
   const KSFactory = await hre.ethers.getContractFactory("KSFactory");
   const KSRouter02 = await hre.ethers.getContractFactory("KSRouter02");
+  const KSZap = await hre.ethers.getContractFactory("KSZap");
 
   const factory = await KSFactory.deploy(feeToS);
   await factory.deployed();
@@ -13,9 +14,12 @@ async function main () {
   const router = await KSRouter02.deploy(factory.address, wethAddress);
   await router.deployed();
 
+  const zap = await KSZap.deploy(factory.address, wethAddress);
+  await zap.deployed();
 
   console.log('Factory deployed to:', factory.address);
   console.log('Router deployed to:', router.address);
+  console.log('Zap deployed to:', zap.address);
 }
 
 main()
